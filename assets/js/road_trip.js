3636f26c2c66f21ddc271f6be7c1e346fa87aa38
Ovie <you@example.com>
2017-12-11 13:20:42 -0500

    fixes timeout

--- assets/js/road_trip.js
+++ assets/js/road_trip.js
@@ -123,8 +123,8 @@ $(document).ready(function() {
     //second Google AJAX call, (?within continue button?)
     $("#continue").on("click", function(e) {
         e.preventDefault();
-        setTimeOut(10000);
-        displayCities();
+        setTimeOut( displayCities(),10000);
+       
     });
 
     // function sets the clicked table row to 'active' and 
